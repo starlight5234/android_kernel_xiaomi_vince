ed5246dc41bc2093b85b14da40b8356ea7786378
Rick Yiu <rickyiu@google.com>
2019-10-05 15:59:06 +0800

    BACKPORT: sched: use sysctl to control energy aware feature
    Will be used to replicate what CAF sched_boost does.
    
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -121,6 +121,8 @@ extern void update_idle_core(struct rq *rq);
 static inline void update_idle_core(struct rq *rq) { }
 #endif
 
+extern bool energy_aware(void);
+
 /*
  * Helpers for converting nanosecond timing to jiffy resolution
  */
@@ -2938,11 +2940,6 @@ static inline void walt_update_min_max_capacity(void) { }
 
 static inline void walt_map_freq_to_load(void) { }
 #endif	/* CONFIG_SCHED_WALT */
-
-static inline bool energy_aware(void)
-{
-	return sched_feat(ENERGY_AWARE);
-}
 
 #define BOOST_KICK      0
 #define CPU_RESERVED    1
